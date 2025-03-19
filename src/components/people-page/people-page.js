ff74a00162bf2733365c08cf3b0c31cc6badfeee
SSQ-Storm <Denislarioshin2004@gmail.com>
2025-03-19 10:59:37 +0500

    Step 12 added hoc components

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import ItemList from '../item-list/item-list';
-import PersonDetails from '../person-details/person-details';
+import ItemList from '../item-list/item-list'
+import ItemDetails, { Record } from '../item-details/item-details'
 
-import './people-page.css';
-import SwapiService from "../../services/swapi-service";
-import Row from "../Row";
-import ErrorBoundary from "../error-boundary";
+import './people-page.css'
+import SwapiService from "../../services/swapi-service"
+import Row from "../Row"
+import ErrorBoundary from "../error-boundary"
 
 
 export default class PeoplePage extends Component {
@@ -14,22 +14,43 @@ export default class PeoplePage extends Component {
     swapiService = new SwapiService()
 
     state = {
-        selectedPerson: null,
+        selectedItem: null,
     }
 
-    onPersonSelected = (selectedPerson) => {
-        this.setState({ selectedPerson })
+    onItemSelected = (selectedItem) => {
+        this.setState({ selectedItem })
     };
 
     render() {
+
+        const {
+            getPerson,
+            getAllPeople,
+            getStarship,
+            getPlanet,
+            getPersonImage,
+            getStarshipImage,
+            getPlanetImage } = this.swapiService
+
         const itemList = (
-            <ItemList onItemSelected={this.onPersonSelected} getData={this.swapiService.getAllPeople}>
+            <ItemList onItemSelected={this.onItemSelected} getData={getAllPeople}>
                 { i => `${i.name} (${i.birthYear})` }
             </ItemList>
         )
 
         const personDetails = (
-            <PersonDetails personId={this.state.selectedPerson} />
+            <ItemDetails
+                itemId={this.state.selectedItem}
+                getData={getPerson}
+                getImageUrl={getPersonImage}>
+
+                <Record field="gender" label="Gender" />
+                <Record field="eyeColor" label="Eye Color" />
+                <Record field="height" label="Height" />
+                <Record field="birthYear" label="Birth Year" />
+
+
+            </ItemDetails>
         )
 
         return (
