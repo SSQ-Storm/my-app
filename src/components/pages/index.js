9638a186d530673eb4b2d1294dfe006234f89204
SSQ-Storm <Denislarioshin2004@gmail.com>
2025-04-02 10:39:51 +0500

    Step 19 refactoring

---
+++ src/components/pages/index.js
@@ -0,0 +1,7 @@
+import PeoplePage from "./people-page"
+ import PlanetsPage from "./planets-page"
+ import StarshipsPage from "./starships-page"
+ 
+export { default as PeoplePage } from './people-page'
+export { default as PlanetsPage } from './planets-page'
+export { default as StarshipsPage } from './starships-page'
--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import App from "./components/app";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
